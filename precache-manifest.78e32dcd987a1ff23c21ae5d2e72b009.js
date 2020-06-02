67f86bd76bf2445ebcccdc72499af9f76720fc0b
Vibhushit07 <vibhushitsinghal80@gmail.com>
2020-06-02 14:54:58 +0530

    Updates

---
+++ precache-manifest.78e32dcd987a1ff23c21ae5d2e72b009.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84a8af99073f68eca312517714edea09",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "3b5e7cd309506e81b3f6",
+    "url": "/covid-19-tracker/static/css/2.b309c102.chunk.css"
+  },
+  {
+    "revision": "40a87376c8483b696e3f",
+    "url": "/covid-19-tracker/static/css/main.caaded5b.chunk.css"
+  },
+  {
+    "revision": "3b5e7cd309506e81b3f6",
+    "url": "/covid-19-tracker/static/js/2.ca234c93.chunk.js"
+  },
+  {
+    "revision": "39ae62bbbe0fcc7b2ccc733de9703168",
+    "url": "/covid-19-tracker/static/js/2.ca234c93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40a87376c8483b696e3f",
+    "url": "/covid-19-tracker/static/js/main.1ee99bb0.chunk.js"
+  },
+  {
+    "revision": "f3669d72bc3ed6fbd813",
+    "url": "/covid-19-tracker/static/js/runtime-main.df328cbd.js"
+  },
+  {
+    "revision": "8666984c0a04fc57a6e89cc041baa673",
+    "url": "/covid-19-tracker/static/media/cover-face.8666984c.jpg"
+  },
+  {
+    "revision": "504096b87f7b71848bbe27f1e268c5e3",
+    "url": "/covid-19-tracker/static/media/doubt.504096b8.PNG"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/covid-19-tracker/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/covid-19-tracker/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/covid-19-tracker/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/covid-19-tracker/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/covid-19-tracker/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "8012cc6d55b153821f33a663708a4a2f",
+    "url": "/covid-19-tracker/static/media/house-cleaing.8012cc6d.jpg"
+  },
+  {
+    "revision": "55acaeab5d0d07d6072530023bc097ca",
+    "url": "/covid-19-tracker/static/media/monitor-health.55acaeab.jpg"
+  },
+  {
+    "revision": "5c74562de235b2bd7ded33dcda04ab27",
+    "url": "/covid-19-tracker/static/media/sneeze.5c74562d.jpg"
+  },
+  {
+    "revision": "a835f781ca01ffcdd9a8d416a631877e",
+    "url": "/covid-19-tracker/static/media/social-distance.a835f781.png"
+  },
+  {
+    "revision": "b1ec7845ec3da2424dec1f6b20dd7342",
+    "url": "/covid-19-tracker/static/media/washing-hands.b1ec7845.jpg"
+  }
+]);
